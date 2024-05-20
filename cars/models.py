024baff02e777674076742159b0686d02db0aaa1
ZAA <zaa.1@mail.ru>
2024-05-20 21:04:08 +0300

    hw_20_1_1

--- cars/models.py
+++ cars/models.py
@@ -2,34 +2,33 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Категоряия', help_text='Введите категорию')
-    description = models.TextField(verbose_name='Описание', help_text='Введите описание', blank=True, null=True)
+    objects = None
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование')
-    description = models.TextField(verbose_name='Описание', help_text='Введите описание')
-    photo = models.ImageField(upload_to='cars/media', blank=True, null=True, verbose_name='Изображение',
-                              help_text='Вставьте Изображение')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория', help_text='Введите '
-                                                                                                          'категорию',
+    objects = None
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='cars/media', blank=True, null=True, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория',
                                  blank=True, null=True, related_name='products')
-    price = models.IntegerField(verbose_name='Цена за покупку', help_text='Введите цену за покупку', blank=True,
+    price = models.IntegerField(verbose_name='Цена за покупку', blank=True,
                                 null=True)
-    created_at = models.DateField(blank=True, null=True, verbose_name='Дата создания')
-    updated_at = models.DateField(blank=True, null=True, verbose_name='Дата последнего изменения')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ['name', 'created_at', 'updated_at']
 
     def __str__(self):
-        return self.name
+        return f'{self.name} {self.description} {self.category} {self.price}'
--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from cars.apps import CarsConfig
-# from cars.views import car
-
 
 app_name = CarsConfig.name
 
